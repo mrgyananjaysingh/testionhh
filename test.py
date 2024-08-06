61eff768f8e960071d0db225ecf0b8f19d23ace5
mrgyananjaysingh <gyananjaysingh2@gmail.com>
2024-08-06 14:55:52 +0530

    list

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("heelo")
+movie=[]
+mov1 = input("entert a first movie")
+mov2 = input("entert a second movie")
+mov3 = input("entert a third movie")
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+print(movie)
